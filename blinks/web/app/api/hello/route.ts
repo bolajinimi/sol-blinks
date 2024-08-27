e6c23f23389010ca1f411267cdf5a2d585f0eff7
bolajinimi <bolajinimi@gmail.com>
2024-08-27 09:21:13 +0100

    blink done

--- blinks/web/app/api/hello/route.ts
+++ blinks/web/app/api/hello/route.ts
@@ -1,37 +1,67 @@
+/* eslint-disable no-global-assign */
 import { ActionGetResponse, ActionPostRequest, ActionPostResponse, ACTIONS_CORS_HEADERS } from "@solana/actions";
-import {PublicKey, SystemProgram, Transaction} from "@solana/web3.js";
+import { clusterApiUrl, Connection, PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
 
 export async function GET(request: Request) {
   const responseBody: ActionGetResponse = {
     icon: "https://cryptologos.cc/logos/solana-sol-logo.png",
-    description: "Hello, World!",
-    title: "Hello, World!",
+    description: "This is solana demo links!",
+    title: "Do blinks!",
     label: "click me!",
-    error: {
-      message: "link not implemented yet",
+    links: {
+      actions: [
+        {
+          href: request.url,
+          label: "same action",
+        },
+        {
+          href: request.url+"?action=another",
+          label: "another action",
+        },
+        
+      ]
     },
+    // error: {
+    //   message: "link not implemented yet",
+    // },
     type: "action",
   };
-
-  // Create and return the Response object in one step
   return new Response(JSON.stringify(responseBody), {
     headers: {
       ...ACTIONS_CORS_HEADERS,
       "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*", // Allow requests from any origin
+      "Access-Control-Allow-Origin": "*", 
     },
   });
 }
 
 export async function POST(request: Request) {
   try {
-    const requestBody: ActionPostRequest = await request.clone().json(); // Clone the request to avoid locking issues
+    const requestBody: ActionPostRequest = await request.clone().json(); 
     const userPubKey = requestBody.account;
     console.log(userPubKey);
 
+    const url = new URL(request.url)
+    const action = url.searchParams.get("action");
+    
+    console.log("performing action "+action)
+
+    const user = new PublicKey(userPubKey); 
+    const connection = new Connection(clusterApiUrl("testnet"));	
+    const ix = SystemProgram.transfer({
+        fromPubkey: user, 
+        toPubkey: new PublicKey('BVqhTFKfBmwuLLGF56FZgd7REnnfY7x6UiFrSMv4r46m'), 
+        lamports: 1
+    });
+
+
     const tx = new Transaction();
-    tx.feePayer = new PublicKey(userPubKey);
-    tx.recentBlockhash = SystemProgram.programId.toBase58();
+    if (action === "another"){
+      tx.add(ix)
+    } 
+    tx.feePayer = user;
+    const bh = (await connection.getLatestBlockhash({commitment: "finalized"})).blockhash;
+    tx.recentBlockhash = bh 
     const serialTx  = tx.serialize({requireAllSignatures: false, verifySignatures: false}).toString('base64');
 
     const response: ActionPostResponse = {
@@ -43,7 +73,7 @@ export async function POST(request: Request) {
     return new Response(JSON.stringify(response), {
       headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*", // Allow requests from any origin
+        "Access-Control-Allow-Origin": "*",
       },
     });
   } catch (error) {
@@ -60,32 +90,7 @@ export async function POST(request: Request) {
 
 export async function OPTIONS(request: Request) {
   return new Response(null, {headers: ACTIONS_CORS_HEADERS});
-  // try {
-  //   const requestBody: ActionPostRequest = await request.clone().json(); // Clone the request to avoid locking issues
-  //   const userPubKey = requestBody.account;
-  //   console.log(userPubKey);
+ 
 
-  //   const response: ActionPostResponse = {
-  //     transaction: "",
-  //     message: "hello " + userPubKey,
-  //   };
-
-  //   // Create and return the Response object in one step
-  //   return new Response(JSON.stringify(response), {
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       "Access-Control-Allow-Origin": "*", // Allow requests from any origin
-  //     },
-  //   });
-  // } catch (error) {
-  //   console.error("Error processing POST request:", error);
-  //   return new Response(JSON.stringify({ error: "Failed to process request" }), {
-  //     status: 500,
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       "Access-Control-Allow-Origin": "*", // Allow requests from any origin
-  //     },
-  //   });
-  // }
 }
 
